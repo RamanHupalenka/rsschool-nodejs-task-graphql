888a3f82945984a11ff1c8a10f5e2e454a515e52
Raman Hupalenka <gupalenko.98@mail.ru>
2023-07-24 21:49:03 +0300

    refactor: apply prettier to qraphql folder files

--- src/routes/graphql/fields/profiles.ts
+++ src/routes/graphql/fields/profiles.ts
@@ -3,8 +3,7 @@ import { EmptyArgs, EmptySource, GraphQLContext } from '../tsTypes/main.js';
 import { ProfileType } from '../objectTypes/profile.js';
 import { profilesResolver } from '../resolvers/profiles.js';
 
-export const profilesField: GraphQLFieldConfig<EmptySource, GraphQLContext, EmptyArgs> =
-  {
-    type: new GraphQLNonNull(new GraphQLList(ProfileType)),
-    resolve: profilesResolver,
-  };
+export const profilesField: GraphQLFieldConfig<EmptySource, GraphQLContext, EmptyArgs> = {
+  type: new GraphQLNonNull(new GraphQLList(ProfileType)),
+  resolve: profilesResolver,
+};
